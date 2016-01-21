1620e721f566d114b46171a87a58aee0ff486383
Jose Garcia-Sacristan <jose.garciascristan@gmail.com>
2016-01-21 23:43:15 +0000

    sequential metex

---
+++ ch2/metex/lib/worker.ex
@@ -0,0 +1,31 @@
+defmodule Metex.Worker do
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} -> "#{location}: #{temp} C"
+      :error -> "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&APPID=b2f087fc3e778f74ce1179b2f80abc13"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+end
